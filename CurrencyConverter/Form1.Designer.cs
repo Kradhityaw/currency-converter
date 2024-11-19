b389c4570a33e75233a55d4950dd7baefe95a89b
aurella11 <cyel526@gmail.com>
2024-11-19 15:54:01 +0700

    First

---
+++ CurrencyConverter/Form1.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace CurrencyConverter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.periodBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.currencyBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.currencyBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.periodBindingSource)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.currencyBindingSource)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.currencyBindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(255, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(322, 38);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Currency Converter";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(59, 116);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Period";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DataSource = this.periodBindingSource;
+            this.comboBox1.DisplayMember = "name";
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(133, 112);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(672, 24);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.ValueMember = "id";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // periodBindingSource
+            // 
+            this.periodBindingSource.DataSource = typeof(CurrencyConverter.Period);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.comboBox2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Location = new System.Drawing.Point(62, 188);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(316, 209);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Origin Amount";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DataSource = this.currencyBindingSource;
+            this.comboBox2.DisplayMember = "abbreviation";
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(223, 94);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(78, 24);
+            this.comboBox2.TabIndex = 2;
+            this.comboBox2.ValueMember = "id";
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // currencyBindingSource
+            // 
+            this.currencyBindingSource.DataSource = typeof(CurrencyConverter.Currency);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(21, 94);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(195, 22);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(18, 66);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 16);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "label3";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.comboBox3);
+            this.groupBox2.Controls.Add(this.textBox2);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Location = new System.Drawing.Point(489, 188);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(316, 209);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Converted To";
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.DataSource = this.currencyBindingSource1;
+            this.comboBox3.DisplayMember = "abbreviation";
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(218, 94);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(78, 24);
+            this.comboBox3.TabIndex = 5;
+            this.comboBox3.ValueMember = "id";
+            this.comboBox3.SelectedIndexChanged += new System.EventHandler(this.comboBox3_SelectedIndexChanged);
+            // 
+            // currencyBindingSource1
+            // 
+            this.currencyBindingSource1.DataSource = typeof(CurrencyConverter.Currency);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Enabled = false;
+            this.textBox2.Location = new System.Drawing.Point(16, 94);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(195, 22);
+            this.textBox2.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 66);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "label4";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(398, 282);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 38);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Swipe";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(864, 459);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CurrencyConverter";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.periodBindingSource)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.currencyBindingSource)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.currencyBindingSource1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.BindingSource periodBindingSource;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.BindingSource currencyBindingSource;
+        private System.Windows.Forms.BindingSource currencyBindingSource1;
+    }
+}
+
---
+++ CurrencyConverter/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CurrencyConverter
+{
+    public partial class Form1 : Form
+    {
+        CurrencyConverterEntities db = new CurrencyConverterEntities();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void GetCrossExchangeRate()
+        {
+            int periodId = (int)comboBox1.SelectedValue;
+            int fromCurrencyId = (int)comboBox2.SelectedValue;
+            int toCurrencyId = (int)comboBox3.SelectedValue;
+
+            decimal rate;
+            decimal total;
+
+            decimal amount = string.IsNullOrEmpty(textBox1.Text) ? 0 : Convert.ToDecimal(textBox1.Text);
+
+            var fromCurrencyRate = db.USDExchangeRates
+                .Where(f => f.currency_id == fromCurrencyId && f.period_id == periodId)
+                .Select(f => f.rate)
+                .FirstOrDefault();
+
+            var toCurrencyRate = db.USDExchangeRates
+                .Where(f => f.currency_id == toCurrencyId && f.period_id == periodId)
+                .Select(f => f.rate)
+                .FirstOrDefault();
+
+            if (fromCurrencyId == 28)
+            {
+                // tidak usah dibagi dengan from karena patokannya sudah 1 USD
+                rate = toCurrencyRate;
+            }
+            else if (toCurrencyId == 28)
+            {
+                // kalau ini kebalikannya, karena di database tidak ada patokan dari USD
+                // karena seluruh patokan di tabel USDExchngeRate itu per 1 USD
+                // karena patokan di tabelnya itu 1 USD itulah mengapa alasannya harus dibagikan dengan 1
+                rate = 1 / fromCurrencyRate;
+            }
+            else
+            {
+                rate = toCurrencyRate / fromCurrencyRate;
+            }
+
+            total = amount * rate;
+
+            textBox2.Text = total.ToString("F2");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            periodBindingSource.DataSource = db.Periods.ToList();
+            currencyBindingSource.DataSource = db.Currencies.ToList();
+            currencyBindingSource1.DataSource = db.Currencies.ToList();
+
+            label3.Text = db.Currencies.FirstOrDefault(f => f.id == (int)comboBox2.SelectedValue).name;
+            label4.Text = db.Currencies.FirstOrDefault(f => f.id == (int)comboBox3.SelectedValue).name;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            GetCrossExchangeRate();
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox2.SelectedItem is Currency currency)
+            {
+                label3.Text = currency.name;
+                GetCrossExchangeRate();
+            }
+        }
+
+        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox3.SelectedItem is Currency currency)
+            {
+                label4.Text = currency.name;
+                GetCrossExchangeRate();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var cb2 = (int)comboBox2.SelectedValue;
+            var cb3 = (int)comboBox3.SelectedValue;
+            var tb1 = textBox1.Text;
+            var tb2 = textBox2.Text;
+            comboBox2.SelectedValue = cb3;
+            comboBox3.SelectedValue = cb2;
+            textBox1.Text = tb2;
+            textBox2.Text = tb1;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            GetCrossExchangeRate();
+        }
+    }
+}
